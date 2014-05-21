63f12a24485581fb46649f4e24f64e7a5fa5a882
chlomac <chloe.macur@inria.fr>
2014-05-21 15:50:50 +0200

    modify typedef structures as learned

--- syscall_data.h
+++ syscall_data.h
@@ -7,93 +7,31 @@
 #define SELECT_FDWR_SET 0x02
 #define SELECT_FDEX_SET 0x04
 
-typedef struct connect_bind_arg_s connect_arg_s;
-typedef connect_arg_s *connect_arg_t;
-
-typedef struct connect_bind_arg_s bind_arg_s;
-typedef bind_arg_s *bind_arg_t;
-
-typedef struct accept_arg_s accept_arg_s;
-typedef accept_arg_s *accept_arg_t;
-
-typedef struct socket_arg_s socket_arg_s;
-typedef socket_arg_s *socket_arg_t;
-
-typedef struct listen_arg_s listen_arg_s;
-typedef listen_arg_s *listen_arg_t;
-
-typedef struct getsockopt_arg_s getsockopt_arg_s;
-typedef getsockopt_arg_s *getsockopt_arg_t;
-
-typedef struct getsockopt_arg_s setsockopt_arg_s;
-typedef setsockopt_arg_s *setsockopt_arg_t;
-
-typedef struct select_arg_s select_arg_s;
-typedef select_arg_s *select_arg_t;
-
-typedef struct poll_arg_s poll_arg_s;
-typedef poll_arg_s *poll_arg_t;
-
-typedef struct recv_arg_s recv_arg_s;
-typedef recv_arg_s *recv_arg_t;
-
-typedef struct recv_arg_s send_arg_s;
-typedef send_arg_s *send_arg_t;
-
-typedef struct sendto_arg_s sendto_arg_s;
-typedef sendto_arg_s *sendto_arg_t;
-
-typedef struct sendto_arg_s recvfrom_arg_s;
-typedef recvfrom_arg_s *recvfrom_arg_t;
-
-typedef struct recvmsg_arg_s sendmsg_arg_s;
-typedef sendmsg_arg_s *sendmsg_arg_t;
-
-typedef struct recvmsg_arg_s recvmsg_arg_s;
-typedef recvmsg_arg_s *recvmsg_arg_t;
-
-typedef struct fcntl_arg_s fcntl_arg_s;
-typedef struct fcntl_arg_s *fcntl_arg_t;
-
-typedef struct write_arg_s write_arg_s;
-typedef write_arg_s *write_arg_t;
-
-typedef struct write_arg_s read_arg_s;
-typedef read_arg_s *read_arg_t;
-
-typedef struct shutdown_arg_s shutdown_arg_s;
-typedef shutdown_arg_s *shutdown_arg_t;
-
-typedef struct getpeername_arg_s getpeername_arg_s;
-typedef getpeername_arg_s *getpeername_arg_t;
-
-typedef struct time_arg_s time_arg_s;
-typedef time_arg_s *time_arg_t;
-
-typedef struct gettimeofday_arg_s gettimeofday_arg_s;
-typedef gettimeofday_arg_s *gettimeofday_arg_t;
-
-typedef struct clockgettime_arg_s clockgettime_arg_s;
-typedef clockgettime_arg_s *clockgettime_arg_t;
-
-
-struct recv_arg_s {
+typedef struct recv_arg_s {
   int sockfd;
   int ret;
   size_t len;
   int flags;
-};
+}recv_arg_s, send_arg_s;
 
-struct recvmsg_arg_s {
+typedef recv_arg_s *recv_arg_t;
+typedef send_arg_s *send_arg_t;
+
+
+typedef struct recvmsg_arg_s {
   int sockfd;
   int ret;
   int len;
   void *data;
   int flags;
   struct msghdr msg;
-};
+}recvmsg_arg_s, sendmsg_arg_s;
 
-struct select_arg_s {
+typedef sendmsg_arg_s *sendmsg_arg_t;
+typedef recvmsg_arg_s *recvmsg_arg_t;
+
+
+typedef struct select_arg_s {
   int fd_state;
   int maxfd;
   int ret;
@@ -101,16 +39,22 @@ struct select_arg_s {
   fd_set fd_write;
   fd_set fd_except;
   double timeout;
-};
+} select_arg_s;
 
-struct poll_arg_s {
+typedef select_arg_s *select_arg_t;
+
+
+typedef struct poll_arg_s {
   int nbfd;
   struct pollfd *fd_list;
   double timeout;
   int ret;
-};
+}poll_arg_s;
 
-struct sendto_arg_s {
+typedef poll_arg_s *poll_arg_t;
+
+
+typedef struct sendto_arg_s {
   int sockfd;
   int ret;
   int len;
@@ -124,9 +68,13 @@ struct sendto_arg_s {
     struct sockaddr_un sau;
     struct sockaddr_nl snl;
   };
-};
+}sendto_arg_s, recvfrom_arg_s;
 
-struct connect_bind_arg_s {
+typedef sendto_arg_s *sendto_arg_t;
+typedef recvfrom_arg_s *recvfrom_arg_t;
+
+
+typedef struct connect_bind_arg_s {
   int sockfd;
   int ret;
   union {
@@ -135,9 +83,13 @@ struct connect_bind_arg_s {
     struct sockaddr_nl snl;
   };
   socklen_t addrlen;
-};
+}connect_arg_s, bind_arg_s;
 
-struct accept_arg_s {
+typedef connect_arg_s *connect_arg_t;
+typedef bind_arg_s *bind_arg_t;
+
+
+typedef struct accept_arg_s {
   int sockfd;
   int ret;
   union {
@@ -148,23 +100,28 @@ struct accept_arg_s {
   socklen_t addrlen;
   void *addr_dest;
   void *len_dest;
-};
+}accept_arg_s;
 
+typedef accept_arg_s *accept_arg_t;
 
-struct socket_arg_s {
+typedef struct socket_arg_s {
   int ret;
   int domain;
   int type;
   int protocol;
-};
+}socket_arg_s;
 
-struct listen_arg_s {
+typedef socket_arg_s *socket_arg_t;
+
+typedef struct listen_arg_s {
   int sockfd;
   int backlog;
   int ret;
-};
+}listen_arg_s;
 
-struct getsockopt_arg_s {
+typedef listen_arg_s *listen_arg_t;
+
+typedef struct getsockopt_arg_s {
   int sockfd;
   int level;
   int optname;
@@ -173,53 +130,75 @@ struct getsockopt_arg_s {
   int ret;
   void *dest;
   void *dest_optlen;
-};
+}getsockopt_arg_s, setsockopt_arg_s;
 
-struct fcntl_arg_s {
+typedef getsockopt_arg_s *getsockopt_arg_t;
+typedef setsockopt_arg_s *setsockopt_arg_t;
+
+
+typedef struct fcntl_arg_s {
   int fd;
   int cmd;
   int arg;                      //TODO put an union to handle various type of argument
   int ret;
-};
+} fcntl_arg_s;
 
-struct write_arg_s {
+typedef struct fcntl_arg_s *fcntl_arg_t;
+
+
+typedef struct write_arg_s {
   int fd;
   int ret;
   int count;
   void *data;
   void *dest;
-};
+} write_arg_s, read_arg_s;
 
-struct shutdown_arg_s {
+typedef write_arg_s *write_arg_t;
+typedef read_arg_s *read_arg_t;
+
+
+typedef struct shutdown_arg_s {
   int fd;
   int how;
   int ret;
-};
+}shutdown_arg_s;
 
-struct getpeername_arg_s {
+typedef shutdown_arg_s *shutdown_arg_t;
+
+
+typedef struct getpeername_arg_s {
   int sockfd;
   struct sockaddr_in in;
   void *sockaddr_dest;
   socklen_t len;
   void *len_dest;
   int ret;
-};
+} getpeername_arg_s;
+
+typedef getpeername_arg_s *getpeername_arg_t;
 
 
-struct time_arg_s {
+typedef struct time_arg_s {
   time_t ret;
-};
+}time_arg_s;
 
-struct gettimeofday_arg_s {
+typedef time_arg_s *time_arg_t;
+
+
+typedef struct gettimeofday_arg_s {
   int ret;
   struct timeval *tv;
   struct timezone *tz;
-};
+}gettimeofday_arg_s;
+typedef gettimeofday_arg_s *gettimeofday_arg_t;
 
-struct clockgettime_arg_s {
+
+typedef struct clockgettime_arg_s {
   int ret;
   struct timespec *tp;
-};
+}clockgettime_arg_s;
+typedef clockgettime_arg_s *clockgettime_arg_t;
 
 typedef union {
   connect_arg_s connect;
