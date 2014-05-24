798c5f059b480a39f7c1fd40f5aa1d43ebed9928
Martin Quinson <martin.quinson@loria.fr>
2014-05-24 11:34:24 +0200

    Various ignorable cosmetics
    - inline start_one_process within start_all_processes
    - reorder the functions in the file to get the main one near the top,
      and the others in order of use
    - reindent everything once again

--- init.c
+++ init.c
@@ -20,8 +20,75 @@ extern xbt_cfg_t _sg_cfg_set;
 
 static void benchmark_matrix_product(float *msec_per_flop);
 static void start_all_processes();
+static void init_station_list();
 
-void init_station_list()
+static inline float str_to_double(const char *string)
+{
+  char *endptr;
+  double value = strtof(string, &endptr);
+  xbt_assert(*endptr == '\0', "%s is not a double", string);
+  return value;
+}
+
+void usage(char *progName)
+{
+  printf("usage : %s [-p flops_power] platform_file.xml deployment_file.xml\n", progName);
+}
+
+void simterpose_init(int argc, char **argv)
+{
+  float msec_per_flop = 0;
+  int flop_option = 0;
+
+  // Initialize SimGrid (and consume the SG command-line options)
+  SD_init(&argc, argv);
+
+  // Initialize simterpose
+  if (argc < 3) {
+    usage(argv[0]);
+    exit(1);
+  } else {
+    int c;
+    while ((c = getopt(argc, argv, "+p:")) != EOF) {
+      switch (c) {
+      case 'p':
+        flop_option = 1;
+        msec_per_flop = 1000000 / str_to_double(optarg);
+        XBT_INFO("Setting reference power to %s flop/s", optarg);
+
+        break;
+
+      default:
+        usage(argv[0]);
+        break;
+      }
+    }
+
+  }
+
+  if (argc - optind < 2) {
+    usage(argv[0]);
+    exit(1);
+  }
+
+  if (!flop_option)
+    benchmark_matrix_product(&msec_per_flop);
+
+
+  init_global_data(msec_per_flop);
+
+  init_socket_gestion();
+  init_comm();
+  cputimer_init();              // creates socket
+
+  SD_create_environment(argv[optind]);
+  parse_deployment_file(argv[optind + 1]);
+
+  init_station_list();
+  start_all_processes();
+}
+
+static void init_station_list()
 {
   xbt_dict_t list_s = simterpose_get_station_list();
   xbt_dict_t list_ip = simterpose_get_ip_list();
@@ -83,120 +150,50 @@ void init_station_list()
   xbt_dynar_free(&no_ip_list);
 }
 
-static inline float str_to_double(const char *string)
-{
-  char *endptr;
-  double value = strtof(string, &endptr);
-  xbt_assert(*endptr == '\0',  "%s is not a double", string);
-  return value;
-}
-
-void usage(char *progName)
-{
-  printf("usage : %s [-p flops_power] platform_file.xml deployment_file.xml\n", progName);
-}
-
-void simterpose_init(int argc, char **argv)
-{
-  float msec_per_flop = 0;
-  int flop_option = 0;
-
-  // Initialize SimGrid (and consume the SG command-line options)
-  SD_init(&argc, argv);
-
-  // Initialize simterpose
-  if (argc < 3) {
-    usage(argv[0]);
-    exit(1);
-  } else {
-    int c;
-    while ((c = getopt(argc, argv, "+p:")) != EOF) {
-      switch (c) {
-      case 'p':
-        flop_option = 1;
-        msec_per_flop = 1000000 / str_to_double(optarg);
-        XBT_INFO("Setting reference power to %s flop/s", optarg);
-
-        break;
-
-      default:
-        usage(argv[0]);
-        break;
-      }
-    }
-
-  }
-
-  if (argc - optind < 2) {
-    usage(argv[0]);
-    exit(1);
-  }
-
-  if (!flop_option)
-    benchmark_matrix_product(&msec_per_flop);
-
-
-  init_global_data(msec_per_flop);
-
-  init_socket_gestion();
-  init_comm();
-  cputimer_init();               // creates socket
-
-  SD_create_environment(argv[optind]);
-  parse_deployment_file(argv[optind + 1]);
-
-  init_station_list();
-  start_all_processes();
-}
-
-static void start_one_process(int rank)
-{
-  int status;
-  int new_pid = fork();
-  if (new_pid == 0) {
-    // in child
-    if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
-      perror("ptrace traceme");
-      exit(1);
-    }
-    
-    xbt_dynar_t cmdline_dynar = parser_get_commandline(rank);
-    char* cmdline_str = xbt_str_join(cmdline_dynar, " ");
-    char **cmdline_array = (char **) xbt_dynar_to_array(cmdline_dynar);
-
-    fprintf(stderr, "Process %d is starting child: %s\n", getpid(), cmdline_str);
-
-    if (execv(cmdline_array[0], cmdline_array) == -1) {
-      fprintf(stderr, "Error while starting %s: %s (full cmdline: %s)\n", cmdline_array[0],strerror(errno),cmdline_str);
-      exit(1);
-    }
-
-  } else {
-    // still in simterpose. wait for the traceme to apply (ie, for child to start)
-    waitpid(new_pid, &status, 0);
-
-    //Trace the child and all upcoming granchilds
-    if (ptrace(PTRACE_SETOPTIONS, new_pid, NULL,
-         PTRACE_O_TRACECLONE | PTRACE_O_TRACEFORK | PTRACE_O_TRACEVFORK | PTRACE_O_TRACEVFORKDONE) 
-        == -1) {
-      perror("Error setoptions");
-      exit(1);
-    }
-    nb_setoptions++;
-   
-    process_set_descriptor(new_pid, process_descriptor_new(parser_get_workstation(rank), new_pid));
-    add_launching_time(new_pid, parser_get_start_time(rank));
-  }
-}
- 
 static void start_all_processes()
 {
   int rank;
 
-  for (rank = 0; rank <parser_get_amount(); rank++) {
-    start_one_process(rank);
-  }
-} 
+  for (rank = 0; rank < parser_get_amount(); rank++) {
+    int status;
+    int new_pid = fork();
+    if (new_pid == 0) {
+      // in child
+      if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
+        perror("ptrace traceme");
+        exit(1);
+      }
+
+      xbt_dynar_t cmdline_dynar = parser_get_commandline(rank);
+      char *cmdline_str = xbt_str_join(cmdline_dynar, " ");
+      char **cmdline_array = (char **) xbt_dynar_to_array(cmdline_dynar);
+
+      fprintf(stderr, "Process %d is starting child: %s\n", getpid(), cmdline_str);
+
+      if (execv(cmdline_array[0], cmdline_array) == -1) {
+        fprintf(stderr, "Error while starting %s: %s (full cmdline: %s)\n", cmdline_array[0], strerror(errno),
+                cmdline_str);
+        exit(1);
+      }
+
+    } else {
+      // still in simterpose. wait for the traceme to apply (ie, for child to start)
+      waitpid(new_pid, &status, 0);
+
+      //Trace the child and all upcoming granchilds
+      if (ptrace(PTRACE_SETOPTIONS, new_pid, NULL,
+                 PTRACE_O_TRACECLONE | PTRACE_O_TRACEFORK | PTRACE_O_TRACEVFORK | PTRACE_O_TRACEVFORKDONE)
+          == -1) {
+        perror("Error setoptions");
+        exit(1);
+      }
+      nb_setoptions++;
+
+      process_set_descriptor(new_pid, process_descriptor_new(parser_get_workstation(rank), new_pid));
+      add_launching_time(new_pid, parser_get_start_time(rank));
+    }
+  }                             // End of loop over all ranks to start
+}
 
 /* Get the power of the current machine from a simple matrix product operation */
 static void benchmark_matrix_product(float *msec_per_flop)
@@ -232,26 +229,27 @@ static void benchmark_matrix_product(float *msec_per_flop)
 
   // run the experiment for real
   cputimer_get(pid, times);
-    long long int initialTime = times[1] + times[2];
-    int i_result, j_result;
+  long long int initialTime = times[1] + times[2];
+  int i_result, j_result;
 
-    for (j_result = 0; j_result < matrixSize; ++j_result) {
-      for (i_result = 0; i_result < matrixSize; ++i_result) {
-        for (i = 0; i < matrixSize; ++i) {
-          matrix_result[j_result][i_result] =
-              matrix_result[i_result][j_result] + matrix1[i_result][i] * matrix2[i][j_result];
-        }
+  for (j_result = 0; j_result < matrixSize; ++j_result) {
+    for (i_result = 0; i_result < matrixSize; ++i_result) {
+      for (i = 0; i < matrixSize; ++i) {
+        matrix_result[j_result][i_result] =
+            matrix_result[i_result][j_result] + matrix1[i_result][i] * matrix2[i][j_result];
       }
     }
+  }
 
-    cputimer_get(pid, times);
-    result = (times[1] + times[2]) - initialTime;
-    XBT_DEBUG("Duration of benchmark : %lld", result);
+  cputimer_get(pid, times);
+  result = (times[1] + times[2]) - initialTime;
+  XBT_DEBUG("Duration of benchmark : %lld", result);
 
-    *msec_per_flop = ((float) result) / (2. * matrixSize * matrixSize * matrixSize);
-    float flop_per_sec = (1000000.) / (*msec_per_flop);
+  *msec_per_flop = ((float) result) / (2. * matrixSize * matrixSize * matrixSize);
+  float flop_per_sec = (1000000.) / (*msec_per_flop);
 
-    XBT_INFO("Your machine was benchmarked at %.0f flop/s (use -p %.0f to avoid that benchmarking)", flop_per_sec, flop_per_sec);
+  XBT_INFO("Your machine was benchmarked at %.0f flop/s (use -p %.0f to avoid that benchmarking)", flop_per_sec,
+           flop_per_sec);
 
   cputimer_exit();
 }
