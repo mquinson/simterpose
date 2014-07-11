0e00d25a4ee9196b4711ed6485fde8a7f824b050
chlomac <chloe.macur@inria.fr>
2014-07-11 10:16:46 +0200

    ok don't indent everything..

--- run_allpairs.sh
+++ run_allpairs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo LD_LIBRARY_PATH = /opt / simgrid / lib /. / simterpose - s plat.xml deploy_allpairs.xml-- log = simterpose.: debug-- log = simix_synchro.: debug-- log = msg.: debug-- log = simix.:debug
-#--log=root.fmt:"'%l: [%c/%p]: %m%n'"
+sudo LD_LIBRARY_PATH=/opt/simgrid/lib/ ./simterpose -s plat.xml deploy_allpairs.xml --log=simterpose.:debug --log=simix_synchro.:debug  --log=msg.:debug --log=simix.:debug  #--log=root.fmt:"'%l: [%c/%p]: %m%n'"
