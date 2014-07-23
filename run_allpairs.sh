4c0449a5fe71786f9231217db9616624c36510fd
chlomac <chloe.macur@inria.fr>
2014-07-23 11:36:29 +0200

    rename allpairs tests

--- run_allpairs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo LD_LIBRARY_PATH=/opt/simgrid/lib/ ./simterpose -s plat.xml deploy_allpairs.xml --log=simterpose.:debug --log=simix_synchro.:debug  --log=msg.:debug --log=simix.:debug  #--log=root.fmt:"'%l: [%c/%p]: %m%n'"
