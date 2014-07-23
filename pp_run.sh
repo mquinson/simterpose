af09aedc8eaf64125c1eacd5da2e92b7303d2255
chlomac <chloe.macur@inria.fr>
2014-07-23 11:38:34 +0200

    new tests: python-pp

---
+++ pp_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo LD_LIBRARY_PATH=/opt/simgrid/lib/ ./simterpose -s plat.xml pp_deploy.xml --log=simterpose.:debug --log=simix_synchro.:debug  --log=msg.:debug --log=simix.:debug #--log=root.fmt:"'%l: [%c/%p]: %m%n'"  # --log=root.fmt:"'[%P on %h]: %m%n'"
