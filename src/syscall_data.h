9465849e2cff59fbd772b277cdb969791c6d4817
Martin Quinson <martin.quinson@loria.fr>
2014-09-24 21:03:05 +0200

    a world about where to find more info on syscalls arguments

--- src/syscall_data.h
+++ src/syscall_data.h
@@ -8,6 +8,25 @@
 #ifndef SYSCALL_DATA_H
 #define SYSCALL_DATA_H
 
+/* Q. Where does this data come from?
+ * A. From the linux source code itself. See for example:
+ *    http://blog.rchapman.org/post/36801038863/linux-system-call-table-for-x86-64
+ *
+ * Excerpt of that page:
+ *
+ * To find the implementation of a system call, grep the kernel tree for SYSCALL_DEFINE.\?(syscall,
+ * For example, to find the read system call:
+ *
+ *   illusion:/usr/src/linux-source-3.2.0$ grep -rA3 'SYSCALL_DEFINE.\?(read,' *
+ *   fs/read_write.c:SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
+ *   fs/read_write.c-{
+ *   fs/read_write.c-        struct file *file;
+ *   fs/read_write.c-        ssize_t ret = -EBADF;
+ *
+ * The results show that the implementation is in fs/read_write.c and that it takes 3 arguments (thus SYSCALL_DEFINE3).
+ *
+ */
+
 #include "sysdep.h"
 
 #define SELECT_FDRD_SET 0x01
