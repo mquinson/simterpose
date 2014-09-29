b78bd85e5047255467eef9fda3474fe5550634bc
Martin Quinson <martin.quinson@loria.fr>
2014-09-29 23:18:41 +0200

    merge syscall_write_{pre,post} into a single handler doing both

--- src/syscall_process.c
+++ src/syscall_process.c
@@ -212,93 +212,90 @@ static int syscall_sendto_post(pid_t pid, reg_s * reg, syscall_arg_u * sysarg, p
 
 /** @brief handle write syscall at the entrance
  *
- * In case of full mediation and if the socket is registered we retrieve the message intended
+ * At entrance, in case of full mediation and if the socket is registered we retrieve the message intended
  * to be written by the application. We send it through MSG and neutralize the real syscall.
  * We don't go to syscall_write_post afterwards.
+ *
+ * At exit, we send the MSG task in order to return control to the MSG process reading the message
  */
-static int syscall_write_pre(reg_s * reg, syscall_arg_u * sysarg, process_descriptor_t * proc)
+static int syscall_write(reg_s * reg, syscall_arg_u * sysarg, process_descriptor_t * proc)
 {
-	proc_inside(proc);
-	XBT_DEBUG(" write_pre");
-	get_args_write(proc, reg, sysarg);
+	if (proc_entering(proc)) {
+		proc_inside(proc);
+		XBT_DEBUG(" write_pre");
+		get_args_write(proc, reg, sysarg);
 
 #ifndef address_translation
-	// XBT_DEBUG("[%d] write_in", pid);
-	if (socket_registered(proc, sysarg->write.fd) != -1) {
-		process_descriptor_t remote_proc;
-		if (process_send_call(proc, sysarg, &remote_proc)) {
-			ptrace_neutralize_syscall(proc->pid);
-
-			write_arg_t arg = &(sysarg->write);
-			ptrace_restore_syscall(proc->pid, SYS_write, arg->ret);
-			if (strace_option)
-				print_write_syscall(proc, sysarg);
-			proc_outside(proc);
-			return PROCESS_TASK_FOUND;
-		}
-	} else {
-		// FIXME: if the socket is not registered, for now we do nothing
-		// and let the kernel run the syscall
-		//XBT_WARN("socket unregistered");
-	}
-#endif
-	return PROCESS_CONTINUE;
-}
-
-/** @brief handle write syscall at the exit
- *
- * We send the MSG task in order to return control to the MSG process reading the message
- */
-static int syscall_write_post(pid_t pid, reg_s * reg, syscall_arg_u * sysarg, process_descriptor_t * proc)
-{
-	proc_outside(proc);
-	XBT_DEBUG("write_post");
-	//    XBT_DEBUG("[%d] write_out", pid);
-	get_args_write(proc, reg, sysarg);
-
-	write_arg_t arg = &(sysarg->write);
-	fd_descriptor_t *file_desc = proc->fd_list[arg->fd];
-	file_desc->ref_nb++;
-
-	// If we're writing into a pipe, we handle things differently
-	if (file_desc != NULL && file_desc->type == FD_PIPE) {
-		if (strace_option)
-			print_write_syscall(proc, sysarg);
-		pipe_t *pipe = file_desc->pipe;
-		if (pipe == NULL)
-			THROW_IMPOSSIBLE;
-
-		pipe_end_t end_in = NULL;
-		xbt_dynar_get_cpy(pipe->read_end, 0, &end_in);
-
-		char buff[256];
-		sprintf(buff, "%d", end_in->fd);
-		msg_host_t receiver = end_in->proc->host;
-
-		XBT_WARN("host %s trying to send to %s in pipe %d (size: %d). Buff = %s", MSG_host_get_name(proc->host),
-				MSG_host_get_name(receiver), end_in->fd, arg->ret, buff);
-
-		double amount = arg->ret;
-		msg_task_t task = MSG_task_create(buff, 0, amount, arg->data);
-		XBT_WARN("hosts: %s send to %s in pipe %d (size: %d)", MSG_host_get_name(proc->host), MSG_host_get_name(receiver),
-				end_in->fd, arg->ret);
-		MSG_task_send(task, buff);
-	} else if (strace_option)
-		print_write_syscall(proc, sysarg);
-
-	file_desc->ref_nb--;
-	file_desc = NULL;
-
-#ifdef address_translation
-	if ((int) reg->ret > 0) {
+		// XBT_DEBUG("[%d] write_in", pid);
 		if (socket_registered(proc, sysarg->write.fd) != -1) {
 			process_descriptor_t remote_proc;
-			if (process_send_call(proc, sysarg, &remote_proc))
+			if (process_send_call(proc, sysarg, &remote_proc)) {
+				ptrace_neutralize_syscall(proc->pid);
+
+				write_arg_t arg = &(sysarg->write);
+				ptrace_restore_syscall(proc->pid, SYS_write, arg->ret);
+				if (strace_option)
+					print_write_syscall(proc, sysarg);
+				proc_outside(proc);
 				return PROCESS_TASK_FOUND;
+			}
+		} else {
+			// FIXME: if the socket is not registered, for now we do nothing
+			// and let the kernel run the syscall
+			//XBT_WARN("socket unregistered");
 		}
-	}
 #endif
-	return PROCESS_CONTINUE;
+		return PROCESS_CONTINUE;
+	} else {
+		proc_outside(proc);
+		XBT_DEBUG("write_post");
+		//    XBT_DEBUG("[%d] write_out", pid);
+		get_args_write(proc, reg, sysarg);
+
+		write_arg_t arg = &(sysarg->write);
+		fd_descriptor_t *file_desc = proc->fd_list[arg->fd];
+		file_desc->ref_nb++;
+
+		// If we're writing into a pipe, we handle things differently
+		if (file_desc != NULL && file_desc->type == FD_PIPE) {
+			if (strace_option)
+				print_write_syscall(proc, sysarg);
+			pipe_t *pipe = file_desc->pipe;
+			if (pipe == NULL)
+				THROW_IMPOSSIBLE;
+
+			pipe_end_t end_in = NULL;
+			xbt_dynar_get_cpy(pipe->read_end, 0, &end_in);
+
+			char buff[256];
+			sprintf(buff, "%d", end_in->fd);
+			msg_host_t receiver = end_in->proc->host;
+
+			XBT_WARN("host %s trying to send to %s in pipe %d (size: %d). Buff = %s", MSG_host_get_name(proc->host),
+					MSG_host_get_name(receiver), end_in->fd, arg->ret, buff);
+
+			double amount = arg->ret;
+			msg_task_t task = MSG_task_create(buff, 0, amount, arg->data);
+			XBT_WARN("hosts: %s send to %s in pipe %d (size: %d)", MSG_host_get_name(proc->host), MSG_host_get_name(receiver),
+					end_in->fd, arg->ret);
+			MSG_task_send(task, buff);
+		} else if (strace_option)
+			print_write_syscall(proc, sysarg);
+
+		file_desc->ref_nb--;
+		file_desc = NULL;
+
+#ifdef address_translation
+		if ((int) reg->ret > 0) {
+			if (socket_registered(proc, sysarg->write.fd) != -1) {
+				process_descriptor_t remote_proc;
+				if (process_send_call(proc, sysarg, &remote_proc))
+					return PROCESS_TASK_FOUND;
+			}
+		}
+#endif
+		return PROCESS_CONTINUE;
+	}
 }
 
 /** @brief handle recvmsg syscall at the entrance
@@ -1846,11 +1843,7 @@ int process_handle(process_descriptor_t * proc, int status)
 			break;
 
 		case SYS_write:
-			if (proc_entering(proc))
-				ret = syscall_write_pre(&arg, sysarg, proc);
-			else
-				ret = syscall_write_post(pid, &arg, sysarg, proc);
-			if (ret)
+			if ((ret = syscall_write(&arg, sysarg, proc)))
 				return ret;
 			break;
 
