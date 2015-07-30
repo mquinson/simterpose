10cd58f5433ec54607cef4d72be4d659f280029c
Louisa-Bessad <louisa.bessad@gmail.com>
2015-07-30 18:15:56 +0200

    File to test fcntl syscall, it does not work

---
+++ tests/fcntl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e # fail fast
+
+make -C ../src/ simterpose
+make -C apps/   fcntl
+
+# Allow to use another folder thant /opt/Simgrid to execute
+sim_dir=$1
+
+# Allow to run under valgrind or gdb easily
+VALGRIND_OPTS="--verbose --trace-children=no --child-silent-after-fork=yes"
+export VALGRIND_OPTS
+
+runner=$2
+
+LD_LIBRARY_PATH=$sim_dir/lib/
+export LD_LIBRARY_PATH
+
+sudo $runner ../src/simterpose -s platform.xml fcntl.xml
